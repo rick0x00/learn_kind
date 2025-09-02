f2577982b2dc2394ee50d6aa57b1816cef30f134
rick0x00 <jose.henrique@incentive.me>
2025-09-02 00:27:32 -0300

    fix(): update ci validation pipeline

--- devops/bootstrap/install_basic_depencencies.sh
+++ devops/bootstrap/install_basic_depencencies.sh
@@ -3,7 +3,10 @@
 apt update && apt install -y wget tar curl
 
 echo "installing task"
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
+curl --location -o install-task.sh https://taskfile.dev/install.sh
+chmod +x install-task.sh
+./install-task.sh -d -b /usr/local/bin
+rm install-task.sh
 
 echo "Insstalling kubeval..."
 cd /tmp/
